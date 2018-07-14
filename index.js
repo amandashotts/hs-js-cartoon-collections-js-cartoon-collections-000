1a3959cc76571ba0a70f3608a9e94ed752ae4c62
Amanda <40045156+amandashotts@users.noreply.github.com>
2018-07-14 12:03:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+var i = 0;
+
 function dwarfRollCall(dwarves) {
+    var numberedDwarves =[];
+        for (i; i < dwarves.length; i++) {
+         numberedDwarves.push(`${i+1}. ${dwarves[i]}`)
+    }
+return numberedDwarves.join(' ')      
 }
+
+function summonCaptainPlanet(planeteerCalls){
+}
+
+function longPlaneteerCalls(words) {
+}
+
+function findTheCheese (foods) {
+}
+
 
 function summonCaptainPlanet(planeteerCalls){
 }

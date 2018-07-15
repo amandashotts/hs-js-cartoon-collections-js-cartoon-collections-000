04cdfbda0bd500c9b231b6adba171b97a684d768
Amanda S <shottsan@dukes.jmu.edu>
2018-07-15 00:53:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-var i = 0;
+
 
 function dwarfRollCall(dwarves) {
-    var numberedDwarves =[];
-        for (i; i < dwarves.length; i++) {
-         numberedDwarves.push(`${i+1}. ${dwarves[i]}`)
+    var numberedDwarves = [];
+        for (var i = 0; i < dwarves.length; i++) {
+         numberedDwarves.push(`${i+1}. ${dwarves[i]} `)
     }
-return numberedDwarves.join(' ')
+return numberedDwarves.join('');
 }
 
 function summonCaptainPlanet(planeteerCalls){
 var excitedPlanets = [];
 for (var i = 0; i < planeteerCalls.length; i++) {
-        excitedPlanets.push(planeteerCalls[i]+'!')
-    }
+    excitedPlanets.push(planeteerCalls[i] + "!");
+}
+
 return excitedPlanets;
-}
-
-function longPlaneteerCalls(words) {
-}
-
-function findTheCheese (foods) {
-}
-
-
-function summonCaptainPlanet(planeteerCalls){
 }
 
 function longPlaneteerCalls(words) {
